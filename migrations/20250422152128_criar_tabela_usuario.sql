d67cadc619a249741aef3ac6a36107f1bd4ba713
Paulo Matos <dev5@wegen.com.br>
2025-04-22 16:06:33 -0300

    ajustes sql inicial

--- migrations/20250422152128_criar_tabela_usuario.sql
+++ migrations/20250422152128_criar_tabela_usuario.sql
@@ -1,7 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
     guid UUID DEFAULT gen_random_uuid() NOT NULL,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    senha_hash VARCHAR(255) NOT NULL
+    email VARCHAR(150) UNIQUE NOT NULL,
+    senha_hash VARCHAR(255) NOT NULL,
+    data_criacao TIMESTAMP DEFAULT NOW() NOT NULL,
+    data_atualizacao TIMESTAMP DEFAULT NOW() NOT NULL
 );
+
+CREATE OR REPLACE FUNCTION atualiza_data_atualizacao()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.data_atualizacao = NOW();
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER atualiza_data_trigger_usuarios
+    BEFORE UPDATE ON usuarios
+    FOR EACH ROW
+    EXECUTE FUNCTION atualiza_data_atualizacao();
